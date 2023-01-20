59ef435707e14f253058d83764b2fdef48457684
Rodrigo Moyano <rodrigomoyano11@gmail.com>
2023-01-19 21:59:15 -0300

    🐞 Se realizaron correcciones generales de "ESlint"

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
 import './globals.css'
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
-    </html>
-  )
-}
+const RootLayout = ({ children }: { children: React.ReactNode }) => (
+  <html lang="en">
+    {/*
+      <head /> will contain the components returned by the nearest parent
+      head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+    */}
+    <head />
+    <body>{children}</body>
+  </html>
+)
+
+export default RootLayout
