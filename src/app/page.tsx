389359b9516976fed9ac240e391ea3c30085b3e5
Rodrigo Moyano <rodrigomoyano11@gmail.com>
2023-02-13 23:38:19 -0300

    🧠 Se implementaron los componentes "MainProvider" y "ThemeProvider" en la página "Root"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,25 @@
 
 import Link from 'next/link'
 
+import { ThemeProvider } from './components/ThemeProvider'
+import { MainProvider } from './contexts/Main'
 import { rootGlobalStyles, rootStyles } from './styles'
 
 const RootPage = () => (
-  <>
-    <h1>Home</h1>
+  <MainProvider value={{ theme: 'dark' }}>
+    <ThemeProvider>
+      <h1>Home</h1>
 
-    <p>For see your pets, please go to</p>
+      <p>For see your pets, please go to</p>
 
-    <Link href="/my-pets">My Pets</Link>
+      <Link href="/my-pets">My Pets</Link>
 
-    <style jsx>{rootStyles}</style>
-    <style jsx global>
-      {rootGlobalStyles}
-    </style>
-  </>
+      <style jsx>{rootStyles}</style>
+      <style jsx global>
+        {rootGlobalStyles}
+      </style>
+    </ThemeProvider>
+  </MainProvider>
 )
 
 export default RootPage
